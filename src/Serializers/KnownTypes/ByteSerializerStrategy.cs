266738ffd6b99a12e7f67cc08f23d97132a9466f
blakely <ablakely@islander.tamucc.edu>
2016-09-08 20:41:01 -0500

    Added Byte SerializerStrategy

---
+++ src/Serializers/KnownTypes/ByteSerializerStrategy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FreecraftCore.Payload.Serializer
+{
+	/// <summary>
+	/// <see cref="ITypeSerializerStrategy"/> for Type <see cref="byte"/>.
+	/// </summary>
+	public class ByteSerializerStrategy : ITypeSerializerStrategy<byte>
+	{
+		/// <summary>
+        /// Perform the steps necessary to serialize the byte.
+        /// </summary>
+        /// <param name="value">The byte to be serialized.</param>
+        /// <param name="dest">The writer entity that is accumulating the output data.</param>
+		public void Write(byte value, IWireMemberWriterStrategy dest)
+		{
+			//Similar to read, this is simple; just write a byte
+			dest.Write(value);
+		}
+		
+		/// <summary>
+        /// Perform the steps necessary to deserialize a byte.
+        /// </summary>
+        /// <param name="source">The reader providing the input data.</param>
+        /// <returns>A byte value from the reader.</returns>
+		public byte Read(IWireMemberReaderStrategy source)
+		{
+			//This is a pretty simple request; just read a byte
+			return source.ReadByte();
+		}
+
+		public ByteSerializerStrategy()
+		{
+			//this serializer needs to subserializers or services.
+		}
+	}
+}
