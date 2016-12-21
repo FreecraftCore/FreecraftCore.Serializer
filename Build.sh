b4b782796b947f2e72cc3893053a664e6f5e2e0d
HelloKitty <andrew.blakely@ymail.com>
2016-12-21 15:00:26 -0600

    Trying to fix build.sh for travis

--- Build.sh
+++ Build.sh
@@ -1,2 +1,5 @@
 dotnet restore
-dotnet build src/*/project.json
+dotnet build -c Debug src/*/project.json
+
+xbuild ./src/FreecraftCore.Serializer.Strategy.Tests/FreecraftCore.Serializer.Strategy.Tests.csproj /p:DebugSymbols=False
+xbuild ./src/FreecraftCore.Serializer.Tests/FreecraftCore.Serializer.Tests.csproj /p:DebugSymbols=False
