4c8a40bf7a5b32a575266fb63ade0cd857bd05da
HelloKitty <andrew.blakely@ymail.com>
2016-12-21 15:05:07 -0600

    Fix travis tests path

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 dotnet restore
 dotnet build -c Debug src/*/project.json
 
-xbuild ./src/FreecraftCore.Serializer.Strategy.Tests/FreecraftCore.Serializer.Strategy.Tests.csproj /p:DebugSymbols=False
-xbuild ./src/FreecraftCore.Serializer.Tests/FreecraftCore.Serializer.Tests.csproj /p:DebugSymbols=False
+xbuild ./tests/FreecraftCore.Serializer.Strategy.Tests/FreecraftCore.Serializer.Strategy.Tests.csproj /p:DebugSymbols=False
+xbuild ./tests/FreecraftCore.Serializer.Tests/FreecraftCore.Serializer.Tests.csproj /p:DebugSymbols=False
