d89da4d231545610b8ab2c7ab2031237375f93fe
HelloKitty <andrew.blakely@ymail.com>
2017-02-28 18:22:47 -0600

    Revert "Temporarily removed myget build scripts"
    This reverts commit 163555961e498cd7296c642931654230c1d244e1.

---
+++ MyGet.bat
@@ -0,0 +1,15 @@
+dotnet pack src/FreecraftCore.Serializer.Stream/ -c Release
+dotnet pack src/FreecraftCore.Serializer.Stream.Default/ -c Release
+dotnet pack src/FreecraftCore.Serializer.API/ -c Release
+dotnet pack src/FreecraftCore.Serializer.Core/ -c Release
+dotnet pack src/FreecraftCore.Serializer.KnownTypes.Decorators/ -c Release
+dotnet pack src/FreecraftCore.Serializer.KnownTypes.Primitives/ -c Release
+dotnet pack src/FreecraftCore.Serializer.KnownTypes.Special/ -c Release
+dotnet pack src/FreecraftCore.Serializer.Metadata/ -c Release
+dotnet pack src/FreecraftCore.Serializer.Metadata.Internal/ -c Release
+dotnet pack src/FreecraftCore.Serializer/ -c Release
+dotnet pack src/FreecraftCore.Serializer.API.Service/ -c Release
+dotnet pack src/FreecraftCore.Serializer.Compression/ -c Release
+
+
+
