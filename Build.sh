6c16b680cc34627dd925c6924291c741c5e2a345
HelloKitty <andrew.blakely@ymail.com>
2017-08-29 09:29:31 -0500

    Try to fix TravisCI

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 dotnet restore
-dotnet build -c Debug src/*/project.json
+dotnet build FreecraftCore.Serializer.sln
 
 xbuild ./tests/FreecraftCore.Serializer.Strategy.Tests/FreecraftCore.Serializer.Strategy.Tests.csproj /p:DebugSymbols=False
 xbuild ./tests/FreecraftCore.Serializer.Tests/FreecraftCore.Serializer.Tests.csproj /p:DebugSymbols=False
