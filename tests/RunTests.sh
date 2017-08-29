573e02f2cd636c639a8b82b27d7f04f028431035
HelloKitty <andrew.blakely@ymail.com>
2017-08-29 10:45:36 -0500

    Try to upgrade nunit in build scripts for Travis

--- tests/RunTests.sh
+++ tests/RunTests.sh
@@ -1,2 +1,2 @@
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./FreecraftCore.Serializer.Strategy.Tests/bin/Debug/FreecraftCore.Serializer.Strategy.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./FreecraftCore.Serializer.Tests/bin/Debug/FreecraftCore.Serializer.Tests.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./FreecraftCore.Serializer.Strategy.Tests/bin/Debug/FreecraftCore.Serializer.Strategy.Tests.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./FreecraftCore.Serializer.Tests/bin/Debug/FreecraftCore.Serializer.Tests.dll
