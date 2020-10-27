e89bb7ffb3e08c0dbfe0e8b6923931724d2b3f1e
HelloKitty <andrew.blakely@ymail.com>
2020-10-27 07:49:38 -0500

    Added BaseArrayTypeSerializerStrategy

---
+++ src/FreecraftCore.Serializer/Serializers/Array/Base/BaseArrayTypeSerializerStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	public abstract class BaseArrayTypeSerializerStrategy<TChildType, TElementType> : StatelessTypeSerializerStrategy<TChildType, TElementType[]> 
+		where TChildType : BaseArrayTypeSerializerStrategy<TChildType, TElementType>, new()
+	{
+
+	}
+}
