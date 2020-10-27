d0f5a8d9c13c54ac3fb5c3d7c106c1e731d2ee87
HelloKitty <andrew.blakely@ymail.com>
2020-10-27 03:20:02 -0500

    Added BaseEncodableTypeSerializerStrategy

---
+++ src/FreecraftCore.Serializer/Serializers/String/Base/BaseEncodableTypeSerializerStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	public abstract class BaseEncodableTypeSerializerStrategy<TChildType> : StatelessTypeSerializer<TChildType, string>
+		where TChildType : StatelessTypeSerializer<TChildType, string>, new()
+	{
+		/// <summary>
+		/// The encoding strategy to use for the serialization.
+		/// </summary>
+		protected Encoding EncodingStrategy { get; }
+
+		/// <summary>
+		/// Size of the individual char encoding.
+		/// </summary>
+		protected int CharacterSize { get; }
+
+		protected BaseEncodableTypeSerializerStrategy([NotNull] Encoding encodingStrategy)
+		{
+			if(encodingStrategy == null) throw new ArgumentNullException(nameof(encodingStrategy));
+
+			EncodingStrategy = encodingStrategy;
+			CharacterSize = new EncoderCharacterSizeStrategy().Compute(encodingStrategy);
+		}
+	}
+}
