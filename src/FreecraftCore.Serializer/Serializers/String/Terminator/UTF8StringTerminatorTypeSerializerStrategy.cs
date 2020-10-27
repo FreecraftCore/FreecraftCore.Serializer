9b92d7d87211a331c30eeb1ddbe5fbfb15642116
HelloKitty <andrew.blakely@ymail.com>
2020-10-27 03:35:20 -0500

    Added string null terminator read/writers

---
+++ src/FreecraftCore.Serializer/Serializers/String/Terminator/UTF8StringTerminatorTypeSerializerStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// UTF8 encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class UTF8StringTerminatorTypeSerializerStrategy : BaseStringTerminatorSerializerStrategy<UTF8StringTerminatorTypeSerializerStrategy>
+	{
+		public UTF8StringTerminatorTypeSerializerStrategy()
+			: base(Encoding.UTF8)
+		{
+
+		}
+	}
+}
---
+++ src/FreecraftCore.Serializer/Serializers/String/Terminator/UnicodeStringTerminatorTypeSerializerStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// Unicode null terminator encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class UnicodeStringTerminatorTypeSerializerStrategy : BaseStringTerminatorSerializerStrategy<UnicodeStringTerminatorTypeSerializerStrategy>
+	{
+		public UnicodeStringTerminatorTypeSerializerStrategy()
+			: base(Encoding.Unicode)
+		{
+
+		}
+	}
+}
