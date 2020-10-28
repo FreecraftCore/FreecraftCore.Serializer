d104a6c6efcc7fb9e709344d3439661e9b49f141
HelloKitty <andrew.blakely@ymail.com>
2020-10-28 12:49:00 -0500

    Implemented ReverseBinaryMutatorStrategy
    Hehe, hope this works. It's complicated stuff!

---
+++ src/FreecraftCore.Serializer/Serializers/Mutators/Order/ReverseBinaryMutatorStrategy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// Binary mutator strategy for reversing a binary chunk/stream.
+	/// See: https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/MemoryExtensions.cs
+	/// </summary>
+	public sealed class ReverseBinaryMutatorStrategy : StatelessBinaryMutatorStrategy<ReverseBinaryMutatorStrategy>
+	{
+		//WRITE
+		public override unsafe void Mutate(Span<byte> source, ref int sourceOffset, Span<byte> destination, ref int destinationOffset)
+		{
+			//See: https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/MemoryExtensions.cs
+			Reverse(source, sourceOffset, destination, destinationOffset);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static unsafe void Reverse(Span<byte> source, int sourceOffset, Span<byte> destination, int destinationOffset)
+		{
+			//See: https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/MemoryExtensions.cs
+			source = source.Slice(sourceOffset);
+			destination = destination.Slice(destinationOffset);
+
+			source.Reverse(); //Not LINQ Reverse, this is a efficient span reverse.
+
+			fixed (byte* sourcePtr = &source.GetPinnableReference())
+			fixed (byte* destPtr = &destination.GetPinnableReference())
+			{
+				//It's possible that the source is the same as the destination, let's not forget that possibility!!
+				if (sourcePtr == destPtr)
+					return;
+
+				Unsafe.CopyBlock(destPtr, sourcePtr, (uint) source.Length);
+			}
+		}
+
+		//READ
+		public override void UnMutate(Span<byte> source, ref int sourceOffset, Span<byte> destination, ref int destinationOffset)
+		{
+			//See: https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/MemoryExtensions.cs
+			Reverse(source, sourceOffset, destination, destinationOffset);
+		}
+	}
+}
