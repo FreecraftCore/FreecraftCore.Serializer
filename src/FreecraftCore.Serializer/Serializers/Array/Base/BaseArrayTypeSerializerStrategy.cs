929e932c3d99b1edb8792e302f198d8b00e28ee7
HelloKitty <andrew.blakely@ymail.com>
2020-10-28 08:07:42 -0500

    Added doc to BaseArrayTypeSerializerStrategy

--- src/FreecraftCore.Serializer/Serializers/Array/Base/BaseArrayTypeSerializerStrategy.cs
+++ src/FreecraftCore.Serializer/Serializers/Array/Base/BaseArrayTypeSerializerStrategy.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace FreecraftCore.Serializer
 {
+	/// <summary>
+	/// Contract for array type serializers.
+	/// </summary>
+	/// <typeparam name="TChildType">The child serializer.</typeparam>
+	/// <typeparam name="TElementType">The element type.</typeparam>
 	public abstract class BaseArrayTypeSerializerStrategy<TChildType, TElementType> : StatelessTypeSerializerStrategy<TChildType, TElementType[]> 
 		where TChildType : BaseArrayTypeSerializerStrategy<TChildType, TElementType>, new()
 	{
