e4971ae8e3bdea8bd88a52b08ee6eda2d710653a
HelloKitty <andrew.blakely@ymail.com>
2020-10-29 09:31:11 -0500

    Added documentation to Emittables

--- src/FreecraftCore.Serializer.Compiler/Emitters/ICompilationUnitEmittable.cs
+++ src/FreecraftCore.Serializer.Compiler/Emitters/ICompilationUnitEmittable.cs
@@ -5,10 +5,21 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace FreecraftCore
 {
+	/// <summary>
+	/// Contract for types that emit a complete and entire compilation unit.
+	/// See: <see cref="CompilationUnitSyntax"/>.
+	/// </summary>
 	public interface ICompilationUnitEmittable
 	{
+		/// <summary>
+		/// The name of the unit.
+		/// </summary>
 		string UnitName { get; }
 
+		/// <summary>
+		/// Creates a compilation unit based on the strategy.
+		/// </summary>
+		/// <returns>A complete compilation unit.</returns>
 		CompilationUnitSyntax CreateUnit();
 	}
 }
--- src/FreecraftCore.Serializer.Compiler/Emitters/IMethodBlockEmittable.cs
+++ src/FreecraftCore.Serializer.Compiler/Emitters/IMethodBlockEmittable.cs
@@ -5,8 +5,16 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace FreecraftCore.Serializer
 {
+	/// <summary>
+	/// Contract for types that emit and entire Method Body Block.
+	/// See: <see cref="BlockSyntax"/>.
+	/// </summary>
 	public interface IMethodBlockEmittable
 	{
+		/// <summary>
+		/// Creates a method body block based on the strategy.
+		/// </summary>
+		/// <returns>A method body block.</returns>
 		BlockSyntax CreateBlock();
 	}
 }
