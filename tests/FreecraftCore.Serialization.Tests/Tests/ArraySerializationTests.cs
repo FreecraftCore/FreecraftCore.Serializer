fb0a5793895a00b36a4ee622ef9643aa8d996c52
HelloKitty <andrew.blakely@ymail.com>
2020-10-29 02:06:01 -0500

    Fixed Array Serialization Tests

--- tests/FreecraftCore.Serialization.Tests/Tests/ArraySerializationTests.cs
+++ tests/FreecraftCore.Serialization.Tests/Tests/ArraySerializationTests.cs
@@ -17,12 +17,13 @@ namespace FreecraftCore.Serialization
 		public void Test_Size_Is_Same_As_Byte_Count(int size)
 		{
 			//arrange
-			SerializerService service = new SerializerService();
-			service.RegisterType<TestNoSizeByteArray>();
-			service.Compile();
+			var serializer = PrimitiveArrayTypeSerializerStrategy<byte>.Instance;
+			Span<byte> buffer = new Span<byte>(new byte[size * 2]);
+			int offset = 0;
 
 			//act
-			byte[] bytes = service.Serialize(new TestNoSizeByteArray(){ Bytes = new byte[size] });
+			serializer.Write(new byte[size], buffer, ref offset);
+			byte[] bytes = buffer.Slice(0, offset).ToArray();
 
 			//assert
 			Assert.NotNull(bytes);
@@ -33,14 +34,16 @@ namespace FreecraftCore.Serialization
 		public void Test_Can_Handle_Large_KnownSize_Array_Objects()
 		{
 			//arrange
-			SerializerService serializer = new SerializerService();
-			serializer.RegisterType<TestLargeKnownSizeArray>();
-			serializer.Compile();
 			byte[] values = Enumerable.Repeat((byte)153, 500).ToArray();
+			var serializer = PrimitiveArrayTypeSerializerStrategy<byte>.Instance;
+			Span<byte> buffer = new Span<byte>(new byte[500 * 2]);
+			int offset = 0;
 
 			//act
-			byte[] bytes = serializer.Serialize(new TestLargeKnownSizeArray(values));
-			TestLargeKnownSizeArray deserialized = serializer.Deserialize<TestLargeKnownSizeArray>(bytes);
+			serializer.Write(values, buffer, ref offset);
+			byte[] bytes = buffer.Slice(0, offset).ToArray();
+			offset = 0;
+			byte[] deserialized = serializer.Read(buffer, ref offset);
 
 			//assert
 			Assert.NotNull(bytes);
@@ -48,36 +51,7 @@ namespace FreecraftCore.Serialization
 			Assert.True(bytes.Length == 500);
 
 			for(int i = 0; i < values.Length; i++)
-				Assert.AreEqual(values[i], deserialized.Bytes[i]);
-		}
-
-
-		[WireDataContract]
-		public class TestNoSizeByteArray
-		{
-			[ReadToEnd]
-			[WireMember(1)]
-			public byte[] Bytes { get; set; }
-		}
-
-		[WireDataContract]
-		public class TestLargeKnownSizeArray
-		{
-			[KnownSize(500)]
-			[WireMember(1)]
-			public byte[] Bytes { get; set; }
-
-			public TestLargeKnownSizeArray(byte[] bytes)
-			{
-				if(bytes == null) throw new ArgumentNullException(nameof(bytes));
-
-				Bytes = bytes;
-			}
-
-			public TestLargeKnownSizeArray()
-			{
-				
-			}
+				Assert.AreEqual(values[i], deserialized[i]);
 		}
 	}
 }
