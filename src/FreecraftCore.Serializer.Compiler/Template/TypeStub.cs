755e06abb265cbeb0203b7b85c66989a62acfa09
HelloKitty <andrew.blakely@ymail.com>
2020-10-29 08:10:56 -0500

    Added template TypeStub

---
+++ src/FreecraftCore.Serializer.Compiler/Template/TypeStub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	internal sealed class TypeStub
+	{
+		public TypeStub()
+		{
+			
+		}
+	}
+}
