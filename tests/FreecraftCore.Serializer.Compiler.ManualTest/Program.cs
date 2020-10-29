ef7aee0fe4999d7260bd72497514bc909e6a2bac
HelloKitty <andrew.blakely@ymail.com>
2020-10-29 09:24:08 -0500

    Added Compiler ManualTest proj

---
+++ tests/FreecraftCore.Serializer.Compiler.ManualTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FreecraftCore.Serializer.Compiler.ManualTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SerializerSourceEmitter emitter = new SerializerSourceEmitter(typeof(SerializerSourceEmitter).Assembly, "");
+			emitter.Generate();
+		}
+	}
+}
