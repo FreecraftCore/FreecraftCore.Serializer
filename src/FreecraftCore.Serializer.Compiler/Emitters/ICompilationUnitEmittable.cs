f15952108a1405c867fb8da300833e6f002ecc55
HelloKitty <andrew.blakely@ymail.com>
2020-10-29 08:10:24 -0500

    Added ICompilationUnitEmittable

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/ICompilationUnitEmittable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FreecraftCore
+{
+	public interface ICompilationUnitEmittable
+	{
+		CompilationUnitSyntax CreateUnit();
+	}
+}
