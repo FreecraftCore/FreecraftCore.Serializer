96312b1c38613806a8974e7d02810ad0f1c6b027
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 04:28:43 -0500

    Added TypeNameStringBuilder

---
+++ src/FreecraftCore.Serializer.Compiler/Builders/TypeNameStringBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	public sealed class TypeNameStringBuilder
+	{
+		public Type ActualType { get; }
+
+		public TypeNameStringBuilder(Type actualType)
+		{
+			ActualType = actualType;
+		}
+
+		public override string ToString()
+		{
+			return GetFriendlyName(ActualType);
+		}
+
+		private static string GetFriendlyName(Type type)
+		{
+			string friendlyName = type.Name;
+			if(type.IsGenericType)
+			{
+				int iBacktick = friendlyName.IndexOf('`');
+				if(iBacktick > 0)
+				{
+					friendlyName = friendlyName.Remove(iBacktick);
+				}
+				friendlyName += "<";
+				Type[] typeParameters = type.GetGenericArguments();
+				for(int i = 0; i < typeParameters.Length; ++i)
+				{
+					string typeParamName = GetFriendlyName(typeParameters[i]);
+					friendlyName += (i == 0 ? typeParamName : "," + typeParamName);
+				}
+				friendlyName += ">";
+			}
+
+			return friendlyName;
+		}
+	}
+}
