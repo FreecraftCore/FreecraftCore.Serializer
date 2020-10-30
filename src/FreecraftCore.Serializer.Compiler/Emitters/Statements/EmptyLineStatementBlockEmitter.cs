219b5cf4a83d593ec8ef655ab187e52fa72e8a40
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 04:43:53 -0500

    Implemented EmptyLineStatementBlockEmitter

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Statements/EmptyLineStatementBlockEmitter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// <see cref="IStatementsBlockEmittable"/> strategy for emitting an empty line statement.
+	/// </summary>
+	public sealed class EmptyLineStatementBlockEmitter : IStatementsBlockEmittable
+	{
+		public List<StatementSyntax> CreateStatements()
+		{
+			return new List<StatementSyntax>
+			{
+				ExpressionStatement
+					(
+						IdentifierName
+						(
+							MissingToken(SyntaxKind.IdentifierToken)
+						)
+					)
+					.WithSemicolonToken
+					(
+						MissingToken(SyntaxKind.SemicolonToken)
+					)
+			};
+		}
+	}
+}
