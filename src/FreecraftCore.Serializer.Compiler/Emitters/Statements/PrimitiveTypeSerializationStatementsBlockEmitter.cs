ba244cc8028358d0c0f6453bbb88631aa2805955
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 06:22:25 -0500

    Refactored PrimitiveTypeSerializationStatementsBlockEmitter

--- src/FreecraftCore.Serializer.Compiler/Emitters/Statements/PrimitiveTypeSerializationStatementsBlockEmitter.cs
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Statements/PrimitiveTypeSerializationStatementsBlockEmitter.cs
@@ -28,103 +28,10 @@ namespace FreecraftCore.Serializer
 			string typeName = new TypeNameStringBuilder(PrimitiveType).ToString();
 
 			//GenericTypePrimitiveSerializerStrategy<int>.Instance.Write(value, destination, ref offset);
-			statements.Add(CreateGenericPrimitiveSerializationStatement(Member.Name, typeName));
+			statements.Add(new RawPrimitiveTypeSerializationGenerator(Member.Name, typeName).Create());
 			statements.AddRange(new EmptyLineStatementBlockEmitter().CreateStatements());
 
 			return statements;
-		}
-
-		private StatementSyntax CreateGenericPrimitiveSerializationStatement([NotNull] string memberName, [NotNull] string primitiveTypeName)
-		{
-			if (memberName == null) throw new ArgumentNullException(nameof(memberName));
-			if (primitiveTypeName == null) throw new ArgumentNullException(nameof(primitiveTypeName));
-
-			return ExpressionStatement
-			(
-				InvocationExpression
-					(
-						MemberAccessExpression
-						(
-							SyntaxKind.SimpleMemberAccessExpression,
-							MemberAccessExpression
-							(
-								SyntaxKind.SimpleMemberAccessExpression,
-								GenericName
-									(
-										Identifier("GenericTypePrimitiveSerializerStrategy")
-									)
-									.WithTypeArgumentList
-									(
-										TypeArgumentList
-										(
-											SingletonSeparatedList<TypeSyntax>
-											(
-												//The generic type input!
-												IdentifierName(primitiveTypeName)
-											)
-										)
-									),
-								IdentifierName("Instance")
-							),
-							IdentifierName("Write")
-						)
-					)
-					.WithArgumentList
-					(
-						ArgumentList
-						(
-							SeparatedList<ArgumentSyntax>
-							(
-								new SyntaxNodeOrToken[]
-								{
-									Argument
-									(
-										//This is the critical part that accesses the member and passed it for serialization.
-										IdentifierName($"{CompilerConstants.SERIALZIABLE_OBJECT_REFERENCE_NAME}.{memberName}")
-									),
-									Token
-									(
-										TriviaList(),
-										SyntaxKind.CommaToken,
-										TriviaList
-										(
-											Space
-										)
-									),
-									Argument
-									(
-										IdentifierName(CompilerConstants.OUTPUT_BUFFER_NAME)
-									),
-									Token
-									(
-										TriviaList(),
-										SyntaxKind.CommaToken,
-										TriviaList
-										(
-											Space
-										)
-									),
-									Argument
-										(
-											IdentifierName(CompilerConstants.OUTPUT_OFFSET_NAME)
-										)
-										.WithRefKindKeyword
-										(
-											Token
-											(
-												TriviaList(),
-												SyntaxKind.RefKeyword,
-												TriviaList
-												(
-													Space
-												)
-											)
-										)
-								}
-							)
-						)
-					)
-			);
 		}
 	}
 }
