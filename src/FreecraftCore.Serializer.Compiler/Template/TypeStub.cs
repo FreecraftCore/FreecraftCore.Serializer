c3d4f594236c070bf2e52054b74b17775fda8045
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 11:28:32 -0500

    Use TypeStub for testing hehe

--- src/FreecraftCore.Serializer.Compiler/Template/TypeStub.cs
+++ src/FreecraftCore.Serializer.Compiler/Template/TypeStub.cs
@@ -5,10 +5,18 @@ using System.Text;
 namespace FreecraftCore.Serializer
 {
 	[WireDataContract]
-	internal sealed class TypeStub
+	public sealed class TypeStub
 	{
 		[WireMember(1)]
 		public int Hello { get; internal set; }
+
+		[SendSize(SendSizeAttribute.SizeType.UInt16)]
+		[Encoding(EncodingType.UTF32)]
+		[WireMember(2)]
+		public string TestString { get; internal set; }
+
+		[WireMember(3)]
+		public ushort HelloAgain { get; internal set; }
 
 		public TypeStub()
 		{
