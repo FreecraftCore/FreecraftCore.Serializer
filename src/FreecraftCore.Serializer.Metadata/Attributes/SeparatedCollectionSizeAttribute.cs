acd4bda88df2b45c09a3f592150aed64ea5c80e1
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 15:29:00 -0500

    Fixed typo in SeparatedCollectionSizeAttribute

--- src/FreecraftCore.Serializer.Metadata/Attributes/SeparatedCollectionSizeAttribute.cs
+++ src/FreecraftCore.Serializer.Metadata/Attributes/SeparatedCollectionSizeAttribute.cs
@@ -12,7 +12,7 @@ namespace FreecraftCore.Serializer
 	/// collection itself in the stream and that it requires higher level handling.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
-	public sealed class SeperatedCollectionSizeAttribute : Attribute
+	public sealed class SeparatedCollectionSizeAttribute : Attribute
 	{
 		/// <summary>
 		/// The name of the collection property.
@@ -25,7 +25,7 @@ namespace FreecraftCore.Serializer
 		public string SizePropertyName { get; }
 
 		/// <inheritdoc />
-		public SeperatedCollectionSizeAttribute([NotNull] string collectionPropertyName, [NotNull] string sizePropertyName)
+		public SeparatedCollectionSizeAttribute([NotNull] string collectionPropertyName, [NotNull] string sizePropertyName)
 		{
 			if(string.IsNullOrWhiteSpace(collectionPropertyName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(collectionPropertyName));
 			if(string.IsNullOrWhiteSpace(sizePropertyName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(sizePropertyName));
