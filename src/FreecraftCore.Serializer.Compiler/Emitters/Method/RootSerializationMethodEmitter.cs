f7148a5226612810828f16e215420c8a19d279a6
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 05:59:44 -0500

    Added RootSerializationMethodBlockEmitter

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Method/RootSerializationMethodEmitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FreecraftCore.Serializer
+{
+	public class RootSerializationMethodBlockEmitter<TSerializableType> : IMethodBlockEmittable
+		where TSerializableType : new()
+	{
+		public BlockSyntax CreateBlock()
+		{
+			//TODO: Figure out how we should decide which strategy to use, right now only simple and flat are supported.
+			//TSerializableType
+			return new FlatComplexTypeSerializationMethodBlockEmitter<TSerializableType>()
+				.CreateBlock();
+		}
+	}
+}
