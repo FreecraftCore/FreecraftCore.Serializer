7c88730e3ad3541e0fc3a58b69353b449ee6849f
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 12:44:39 -0500

    Implemented KnownSizeStringSerializerHelper Read

--- src/FreecraftCore.Serializer/Serializers/String/Sizing/KnownSizeStringSerializerHelper.cs
+++ src/FreecraftCore.Serializer/Serializers/String/Sizing/KnownSizeStringSerializerHelper.cs
@@ -8,6 +8,28 @@ namespace FreecraftCore.Serializer
 	public static class KnownSizeStringSerializerHelper
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static string Read(Span<byte> source, ref int offset,
+			int fixedSize, EncodingType encodingType, bool shouldTerminate) //optional args
+		{
+			if (fixedSize == 0)
+				return String.Empty;
+
+			switch (encodingType)
+			{
+				case EncodingType.ASCII:
+					return ReadFixedLength(ASCIIStringTypeSerializerStrategy.Instance, ASCIIStringTerminatorTypeSerializerStrategy.Instance, source, ref offset, fixedSize, shouldTerminate);
+				case EncodingType.UTF16:
+					return ReadFixedLength(UTF16StringTypeSerializerStrategy.Instance, UTF16StringTerminatorTypeSerializerStrategy.Instance, source, ref offset, fixedSize, shouldTerminate);
+				case EncodingType.UTF32:
+					return ReadFixedLength(UTF32StringTypeSerializerStrategy.Instance, UTF32StringTerminatorTypeSerializerStrategy.Instance, source, ref offset, fixedSize, shouldTerminate);
+				case EncodingType.UTF8:
+					return ReadFixedLength(UTF8StringTypeSerializerStrategy.Instance, UTF8StringTerminatorTypeSerializerStrategy.Instance, source, ref offset, fixedSize, shouldTerminate);
+				default:
+					throw new ArgumentOutOfRangeException(nameof(encodingType), encodingType, null);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void Write(string value, Span<byte> destination, ref int offset, 
 			int fixedSize, EncodingType encodingType, bool shouldTerminate) //optional args
 		{
@@ -35,17 +57,39 @@ namespace FreecraftCore.Serializer
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static string ReadFixedLength<TStringSerializerStrategy, TTerminatorStrategy>(TStringSerializerStrategy serializer, TTerminatorStrategy terminatorSerializer,
+			Span<byte> source, ref int offset, int fixedSize, bool shouldTerminate)
+			where TStringSerializerStrategy : BaseStringTypeSerializerStrategy<TStringSerializerStrategy>, new()
+			where TTerminatorStrategy : BaseStringTerminatorSerializerStrategy<TTerminatorStrategy>, new()
+		{
+			int initialOffset = offset;
+			int fixedSizeLength = serializer.CharacterSize * fixedSize;
+			if(shouldTerminate)
+				fixedSizeLength -= serializer.CharacterSize;
+
+			string value = serializer.Read(source.Slice(0, offset + fixedSizeLength + offset), ref offset);
+
+			//Did we read enough?? Doesn't matter, we can direct set to new position.
+			offset = initialOffset + fixedSizeLength;
+
+			if(shouldTerminate)
+				terminatorSerializer.Read(source, ref offset);
+
+			return value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private static void WriteFixedLength<TStringSerializerStrategy, TTerminatorStrategy>(TStringSerializerStrategy serializer, TTerminatorStrategy terminatorSerializer, 
 			string value, Span<byte> destination, ref int offset, int fixedSize, bool shouldTerminate)
 			where TStringSerializerStrategy : BaseStringTypeSerializerStrategy<TStringSerializerStrategy>, new()
 			where TTerminatorStrategy : BaseStringTerminatorSerializerStrategy<TTerminatorStrategy>, new()
 		{
-			int fixedSizeLength = serializer.CharacterSize * fixedSize + offset;
+			int fixedSizeLength = serializer.CharacterSize * fixedSize;
 			if (shouldTerminate)
 				fixedSizeLength -= serializer.CharacterSize;
 
 			int lastOffset = offset;
-			serializer.Write(value, destination.Slice(0, fixedSizeLength), ref offset);
+			serializer.Write(value, destination.Slice(0, fixedSizeLength + offset), ref offset);
 
 			//Force the fixed length buffer write, and null the buffer out (could be data in it otherwise)
 			while(offset < lastOffset)
