3c90bd5f8ce2eb28d88389b24a26f8649bc44565
HelloKitty <andrew.blakely@ymail.com>
2020-10-30 13:05:41 -0500

    Added RawKnownSizeStringTypeSerializerGenerator

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Statements/Raw/RawKnownSizeStringTypeSerializerGenerator.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace FreecraftCore.Serializer
+{
+	public sealed class RawKnownSizeStringTypeSerializerGenerator
+	{
+		public string MemberName { get; }
+
+		public EncodingType Encoding { get; }
+
+		public int FixedSizeValue { get; }
+
+		public bool ShouldTerminate { get; }
+
+		public RawKnownSizeStringTypeSerializerGenerator([NotNull] string memberName, EncodingType encoding, int fixedSizeValue, bool shouldTerminate)
+		{
+			if (!Enum.IsDefined(typeof(EncodingType), encoding)) throw new InvalidEnumArgumentException(nameof(encoding), (int) encoding, typeof(EncodingType));
+			if (fixedSizeValue <= 0) throw new ArgumentOutOfRangeException(nameof(fixedSizeValue));
+			MemberName = memberName ?? throw new ArgumentNullException(nameof(memberName));
+
+			Encoding = encoding;
+			FixedSizeValue = fixedSizeValue;
+			ShouldTerminate = shouldTerminate;
+		}
+
+		public StatementSyntax Create()
+		{
+			return ExpressionStatement
+			(
+				InvocationExpression
+					(
+						MemberAccessExpression
+						(
+							SyntaxKind.SimpleMemberAccessExpression,
+							IdentifierName(nameof(KnownSizeStringSerializerHelper)),
+							IdentifierName(nameof(KnownSizeStringSerializerHelper.Write))
+						)
+					)
+					.WithArgumentList
+					(
+						ArgumentList
+						(
+							SeparatedList<ArgumentSyntax>
+							(
+								new SyntaxNodeOrToken[]
+								{
+									Argument
+									(
+										//This is the critical part that accesses the member and passed it for serialization.
+										IdentifierName($"{CompilerConstants.SERIALZIABLE_OBJECT_REFERENCE_NAME}.{MemberName}")
+									),
+									Token
+									(
+										TriviaList(),
+										SyntaxKind.CommaToken,
+										TriviaList
+										(
+											Space
+										)
+									),
+									Argument
+									(
+										IdentifierName(CompilerConstants.OUTPUT_BUFFER_NAME)
+									),
+									Token
+									(
+										TriviaList(),
+										SyntaxKind.CommaToken,
+										TriviaList
+										(
+											Space
+										)
+									),
+									Argument
+										(
+											IdentifierName(CompilerConstants.OUTPUT_OFFSET_NAME)
+										)
+										.WithRefKindKeyword
+										(
+											Token
+											(
+												TriviaList(),
+												SyntaxKind.RefKeyword,
+												TriviaList
+												(
+													Space
+												)
+											)
+										),
+									Token
+									(
+										TriviaList(),
+										SyntaxKind.CommaToken,
+										TriviaList
+										(
+											Space
+										)
+									),
+									Argument
+									(
+										LiteralExpression
+										(
+											SyntaxKind.NumericLiteralExpression,
+											Literal(FixedSizeValue)
+										)
+									),
+									Token
+									(
+										TriviaList(),
+										SyntaxKind.CommaToken,
+										TriviaList
+										(
+											Space
+										)
+									),
+									Argument
+									(
+										MemberAccessExpression
+										(
+											SyntaxKind.SimpleMemberAccessExpression,
+											IdentifierName(nameof(EncodingType)),
+											IdentifierName(Encoding.ToString())
+										)
+									),
+									Token
+									(
+										TriviaList(),
+										SyntaxKind.CommaToken,
+										TriviaList
+										(
+											Space
+										)
+									),
+									Argument
+									(
+										LiteralExpression
+										(
+											//Fancy pants way to indicate to the helper if we should terminate
+											ShouldTerminate ? SyntaxKind.TrueLiteralExpression : SyntaxKind.FalseLiteralExpression
+										)
+									)
+								}
+							)
+						)
+					)
+			);
+		}
+	}
+}
