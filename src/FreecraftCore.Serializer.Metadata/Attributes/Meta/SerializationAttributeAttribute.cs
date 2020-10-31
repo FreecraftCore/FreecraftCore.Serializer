161377bfbe35bdce77122cd9916fa0dfe65974fb
HelloKitty <andrew.blakely@ymail.com>
2020-10-31 11:55:27 -0500

    Added SerializationAttributeAttribute meta attribute

---
+++ src/FreecraftCore.Serializer.Metadata/Attributes/Meta/SerializationAttributeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// <see cref="Attribute"/> for marking a target <see cref="Attribute"/>
+	/// class <see cref="Type"/> as a serialization controlling attribute.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class SerializationAttributeAttribute : Attribute
+	{
+		public SerializationAttributeAttribute()
+		{
+			
+		}
+	}
+}
