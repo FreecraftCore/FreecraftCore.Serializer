0fd8d9a6518d57621b41678ec2e34cd79807a833
HelloKitty <andrew.blakely@ymail.com>
2020-10-31 12:42:05 -0500

    Added EnumTypeSerializerStatementsBlockEmitter

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Statements/EnumTypeSerializerStatementsBlockEmitter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FreecraftCore.Serializer
+{
+	public sealed class EnumTypeSerializerStatementsBlockEmitter : BaseSerializationStatementsBlockEmitter
+	{
+		public EnumTypeSerializerStatementsBlockEmitter([NotNull] Type primitiveType, [NotNull] MemberInfo member) : base(primitiveType, member)
+		{
+
+		}
+
+		public override List<StatementSyntax> CreateStatements()
+		{
+			List<StatementSyntax> statements = new List<StatementSyntax>();
+
+			EnumStringAttribute enumStringAttri = Member.GetCustomAttribute<EnumStringAttribute>();
+
+			//TODO: Support custom primitivie specifier.
+			//Primitive serialization YAY
+			if (enumStringAttri == null)
+			{
+				var generator = new RawEnumPrimitiveSerializationGenerator(Member.Name, PrimitiveType.GetEnumUnderlyingType(), PrimitiveType);
+				statements.Add(generator.Create());
+			}
+
+			return statements;
+		}
+	}
+}
