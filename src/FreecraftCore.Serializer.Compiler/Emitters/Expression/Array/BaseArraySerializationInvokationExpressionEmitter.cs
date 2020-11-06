5994f3987380f83a7e72220a1048ee5929868bf4
HelloKitty <andrew.blakely@ymail.com>
2020-11-06 16:19:05 -0600

    Added BaseArraySerializationInvokationExpressionEmitter

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Expression/Array/BaseArraySerializationInvokationExpressionEmitter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace FreecraftCore.Serializer
+{
+	internal abstract class BaseArraySerializationInvokationExpressionEmitter<TSerializerTypeNameType> : IInvokationExpressionEmittable
+		where TSerializerTypeNameType : BaseArraySerializerNonGenericMarker
+	{
+		public InvocationExpressionSyntax Create()
+		{
+			return InvocationExpression
+			(
+				MemberAccessExpression
+					(
+						SyntaxKind.SimpleMemberAccessExpression,
+						MemberAccessExpression
+							(
+								SyntaxKind.SimpleMemberAccessExpression,
+								GenericName
+									(
+										Identifier(typeof(TSerializerTypeNameType).Name)
+									)
+									.WithTypeArgumentList
+									(
+										TypeArgumentList
+											(
+												SeparatedList<TypeSyntax>
+												(
+													CalculateGenericParameterList()
+												)
+											)
+											.WithLessThanToken
+											(
+												Token(SyntaxKind.LessThanToken)
+											)
+											.WithGreaterThanToken
+											(
+												Token(SyntaxKind.GreaterThanToken)
+											)
+									),
+								IdentifierName(nameof(PrimitiveArrayTypeSerializerStrategy<int>.Instance))
+							)
+							.WithOperatorToken
+							(
+								Token(SyntaxKind.DotToken)
+							),
+						IdentifierName(nameof(PrimitiveArrayTypeSerializerStrategy<int>.Instance.Write))
+					)
+					.WithOperatorToken
+					(
+						Token(SyntaxKind.DotToken)
+					)
+			);
+		}
+
+		private IEnumerable<SyntaxNodeOrToken> CalculateGenericParameterList()
+		{
+			SyntaxNodeOrToken[] parameters = CalculateGenericTypeParameters()
+				.ToArray();
+
+			if (parameters.Length < 2)
+				yield return parameters[0];
+
+			//Yield first, rest should have comma tokens
+			yield return parameters[0];
+
+			foreach (SyntaxNodeOrToken entry in parameters)
+			{
+				yield return Token(SyntaxKind.CommaToken);
+				yield return entry;
+			}
+		}
+
+		protected abstract IEnumerable<SyntaxNodeOrToken> CalculateGenericTypeParameters();
+	}
+}
