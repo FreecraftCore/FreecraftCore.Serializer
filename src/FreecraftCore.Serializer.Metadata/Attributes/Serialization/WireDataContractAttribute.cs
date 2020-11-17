07ddb1a641f69c25b5e13c759838ebdef073ed61
HelloKitty <andrew.blakely@ymail.com>
2020-11-17 08:20:45 -0600

    Removed InformationFlags from BaseType registeration

--- src/FreecraftCore.Serializer.Metadata/Attributes/Serialization/WireDataContractAttribute.cs
+++ src/FreecraftCore.Serializer.Metadata/Attributes/Serialization/WireDataContractAttribute.cs
@@ -11,57 +11,26 @@ namespace FreecraftCore.Serializer
 	[AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum, Inherited = true, AllowMultiple = false)] //classes or structs can be WireDataContracts. Inteffaces now can be too.
 	public class WireDataContractAttribute : Attribute
 	{
-		public enum KeyType
-		{
-			None = 0,
-			Byte = 1,
-			UShort = 2,
-			Int32 = 3
-		}
-
 		/// <summary>
 		/// Indicates the size of the key.
 		/// (Ex. Send only a byte to indicate type or maybe define it using a full Int32)
 		/// </summary>
-		public KeyType OptionalChildTypeKeySize { get; }
+		public PrimitiveSizeType? OptionalSubTypeKeySize { get; }
 
 		/// <summary>
-		/// Indicates if the type information used for mapping base to child should
-		/// be consumed when encountered.
+		/// Indicates if SubType size is used.
 		/// </summary>
-		public InformationHandlingFlags TypeHandling { get; }
+		public bool UsesSubTypeSize => OptionalSubTypeKeySize.HasValue;
 
-		/// <summary>
-		/// Indicates if the type should expect to be linked to at runtime.
-		/// </summary>
-		public bool ExpectRuntimeLink { get; }
-
-		public WireDataContractAttribute(KeyType keyType, bool expectRuntimeLink)
-			: this(keyType, InformationHandlingFlags.Default, expectRuntimeLink)
+		public WireDataContractAttribute(PrimitiveSizeType optionalSubtypeKeySize)
 		{
-			
+			if (!Enum.IsDefined(typeof(PrimitiveSizeType), optionalSubtypeKeySize)) throw new InvalidEnumArgumentException(nameof(optionalSubtypeKeySize), (int) optionalSubtypeKeySize, typeof(PrimitiveSizeType));
+			OptionalSubTypeKeySize = optionalSubtypeKeySize;
 		}
 
-		/// <summary>
-		/// Creates a new Meta-data attribute indicating the type is a WireDataContract.
-		/// </summary>
-		public WireDataContractAttribute(KeyType keyType = KeyType.None, InformationHandlingFlags typeHandling = InformationHandlingFlags.Default, bool expectRuntimeLink = false)
+		public WireDataContractAttribute()
 		{
-			if (!Enum.IsDefined(typeof(KeyType), keyType))
-				throw new ArgumentException($"Provided enum argument {nameof(keyType)} of Type {typeof(KeyType)} with value {keyType} was not in valid range.", nameof(keyType));
-
-			int i;
-
-			if (!Enum.IsDefined(typeof(InformationHandlingFlags), typeHandling) && Int32.TryParse(typeHandling.ToString(), out i))
-				throw new ArgumentException($"Provided enum argument {nameof(typeHandling)} of Type {typeof(InformationHandlingFlags)} with value {typeHandling} was not in valid range.", nameof(typeHandling));
-
-			//The keytype provided is optional to be none.
-			//If you want to wire up children to this for polymorphic serialization then
-			//A provided keysize must be declared on the 1 WireDataContract instead of the N many WireBaseType attributes
-
-			OptionalChildTypeKeySize = keyType;
-			TypeHandling = typeHandling;
-			ExpectRuntimeLink = expectRuntimeLink;
+			OptionalSubTypeKeySize = null;
 		}
 	}
 }
--- tests/FreecraftCore.Serialization.Tests/RealWorld/BoomaProxyPacketPayloads.cs
+++ tests/FreecraftCore.Serialization.Tests/RealWorld/BoomaProxyPacketPayloads.cs
@@ -106,14 +106,13 @@ namespace FreecraftCore.Serialization.Tests.RealWorld
 	/// type based on a 2 byte opcode <see cref="ushort"/> that comes over the network.
 	/// </summary>
 	[DefaultChild(typeof(UnknownPatchPacket))] //this will be the default deserialized packet when we don't know what it is.
-	[WireDataContract(WireDataContractAttribute.KeyType.UShort, InformationHandlingFlags.DontConsumeRead, true)]
+	[WireDataContract(PrimitiveSizeType.UInt16)]
 	public abstract class PSOBBPatchPacketPayloadServer
 	{
 		//We really only add this because sometimes we'll get a packet we don't know about and we'll want to log about it.
 		/// <summary>
 		/// The operation code of the packet.
 		/// </summary>
-		[DontWrite]
 		[WireMember(1)]
 		public short OperationCode { get; }
 
@@ -156,10 +155,12 @@ namespace FreecraftCore.Serialization.Tests.RealWorld
 	/// The base type for PSOBB patching packet payloads that the client sends. This isn't for ship payloads The child
 	/// type based on a 2 byte opcode <see cref="ushort"/> that comes over the network.
 	/// </summary>
-	[WireDataContract(WireDataContractAttribute.KeyType.UShort, true)]
+	[WireDataContract(PrimitiveSizeType.UInt16)]
 	public abstract class PSOBBPatchPacketPayloadClient
 	{
 		//Nothing, only the 2 byte Type is relevant for this base packet.
+		[WireMember(1)]
+		public short OperationCode { get; }
 
 		protected PSOBBPatchPacketPayloadClient()
 		{
