e59ceaf387cdf7af83d996a1ba262f0822a0dc92
HelloKitty <andrew.blakely@ymail.com>
2020-11-18 06:32:14 -0600

    Implemented IWireMessage

---
+++ src/FreecraftCore.Serializer.Metadata/Serializer/ISelfSerializable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Contract for types that are self-serializable.
+	/// Means they can read/write their binary representation.
+	/// Basically it means they are their own <see cref="ITypeSerializerStrategy{T}"/>
+	/// implementation.
+	/// </summary>
+	/// <typeparam name="TMessageType"></typeparam>
+	public interface ISelfSerializable<TMessageType> : ITypeSerializerStrategy<TMessageType>
+		where TMessageType : ISelfSerializable<TMessageType>
+	{
+		
+	}
+}
---
+++ src/FreecraftCore.Serializer.Metadata/Serializer/IWireMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// Contract for top-level serializable types.
+	/// Think of this as "messages" a complete Type that encloses around fields (object graph)
+	/// that can be read/written.
+	///
+	/// This contract is not required for a Type to be serializable. Only <see cref="WireDataContractAttribute"/>
+	/// is required for that. However, for the Serializer service to know how to read/write the Type it is required
+	/// that this type be implemented.
+	/// </summary>
+	/// <typeparam name="TMessageType">Wire message type.</typeparam>
+	public interface IWireMessage<TMessageType> : ISelfSerializable<TMessageType>
+		where TMessageType : IWireMessage<TMessageType>
+	{
+
+	}
+}
