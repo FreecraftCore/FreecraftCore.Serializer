e0c806e9d6aa08b1e23aaad37be05e1f47d2ccf3
HelloKitty <andrew.blakely@ymail.com>
2020-11-18 06:56:11 -0600

    Added non-generic ISelfSerializable

--- src/FreecraftCore.Serializer.Metadata/Serializer/ISelfSerializable.cs
+++ src/FreecraftCore.Serializer.Metadata/Serializer/ISelfSerializable.cs
@@ -7,13 +7,21 @@ using FreecraftCore.Serializer;
 namespace FreecraftCore
 {
 	/// <summary>
+	/// Metadata marker interface.
+	/// </summary>
+	public interface ISelfSerializable
+	{
+
+	}
+
+	/// <summary>
 	/// Contract for types that are self-serializable.
 	/// Means they can read/write their binary representation.
 	/// Basically it means they are their own <see cref="ITypeSerializerStrategy{T}"/>
 	/// implementation.
 	/// </summary>
 	/// <typeparam name="TMessageType"></typeparam>
-	public interface ISelfSerializable<TMessageType> : ITypeSerializerStrategy<TMessageType>
+	public interface ISelfSerializable<TMessageType> : ITypeSerializerStrategy<TMessageType>, ISelfSerializable
 		where TMessageType : ISelfSerializable<TMessageType>
 	{
 		
