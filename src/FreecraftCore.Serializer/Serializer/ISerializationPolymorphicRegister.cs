3ef10341d50e2baee978efed474a1a122dbd5afa
HelloKitty <andrew.blakely@ymail.com>
2020-11-18 22:55:45 -0600

    Added ISerializationPolymorphicRegister

---
+++ src/FreecraftCore.Serializer/Serializer/ISerializationPolymorphicRegister.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	public interface ISerializationPolymorphicRegister
+	{
+		/// <summary>
+		/// Registered a polymorphic serializer that can handle deserialization
+		/// of abstract or polymorphic supported types.
+		///
+		/// This method should only be called at startup. It is not safe to call this method in the middle of using
+		/// the serializer.
+		/// </summary>
+		/// <typeparam name="TSerializerType">The serializer type.</typeparam>
+		void RegisterPolymorphicSerializer<TSerializerType>()
+			where TSerializerType : ITypeSerializerStrategy, new();
+	}
+}
