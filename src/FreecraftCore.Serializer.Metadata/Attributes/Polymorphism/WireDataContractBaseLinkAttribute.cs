4ae60406b8a06191c1e799ee8053b8eb696cd880
HelloKitty <andrew.blakely@ymail.com>
2020-11-19 18:40:23 -0600

    Fixed documentation on WireDataContractBaseLinkAttribute

--- src/FreecraftCore.Serializer.Metadata/Attributes/Polymorphism/WireDataContractBaseLinkAttribute.cs
+++ src/FreecraftCore.Serializer.Metadata/Attributes/Polymorphism/WireDataContractBaseLinkAttribute.cs
@@ -6,20 +6,21 @@ using JetBrains.Annotations;
 namespace FreecraftCore.Serializer
 {
 	/// <summary>
-	/// Metadata marker for Types that are linked to their base type but allow for
-	/// registration to be done at runtime.
+	/// Metadata marker for Types that are linked to their base type
+	/// via a key. Polymorphic types can be serialized directly without a link to
+	/// their base information since all child's implement serialization for base-type fields.
+	/// However, it's required for deserialization/read to know the linking.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)] //classes or structs can be WireDataContracts
 	public class WireDataContractBaseLinkAttribute : Attribute
 	{
 		/// <summary>
-		/// Unique index/key for the interpter to know what the base type is in the stream.
+		/// Unique index/key that links the child to the base type.
 		/// </summary>
 		public int Index { get; }
 
 		/// <summary>
 		/// Links to the basetype with the provided index at runtime.
-		/// Will requite you to manually link with the serializer before compiling.
 		/// </summary>
 		/// <param name="index">Unique per Type index.</param>
 		public WireDataContractBaseLinkAttribute(int index)
