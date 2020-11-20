ba015d8d97f01eff35da61e8d3fa1c52b33b8dbf
HelloKitty <andrew.blakely@ymail.com>
2020-11-20 05:42:45 -0600

    Extracted Wire message implementation from large source generator

---
+++ src/FreecraftCore.Serializer.Compiler/Emitters/IMemberDeclarationEmittable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FreecraftCore.Serializer
+{
+	public interface IMemberDeclarationEmittable
+	{
+		/// <summary>
+		/// Creates a method body block based on the strategy.
+		/// </summary>
+		/// <returns>A method body block.</returns>
+		MemberDeclarationSyntax Create();
+	}
+}
