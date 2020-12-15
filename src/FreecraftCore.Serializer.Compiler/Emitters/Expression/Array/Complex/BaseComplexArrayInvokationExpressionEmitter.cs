b672225f742aefaf021b4d15828b75e34c53331e
HelloKitty <andrew.blakely@ymail.com>
2020-12-14 22:22:16 -0600

    Moved TypeNameTypeCollector

--- src/FreecraftCore.Serializer.Compiler/Emitters/Expression/Array/Complex/BaseComplexArrayInvokationExpressionEmitter.cs
+++ src/FreecraftCore.Serializer.Compiler/Emitters/Expression/Array/Complex/BaseComplexArrayInvokationExpressionEmitter.cs
@@ -11,23 +11,6 @@ using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace FreecraftCore.Serializer
 {
-	internal class TypeNameTypeCollector : CSharpSyntaxWalker
-	{
-		public List<NameSyntax> Types { get; } = new List<NameSyntax>();
-
-		public override void VisitIdentifierName(IdentifierNameSyntax node)
-		{
-			if (node.ToFullString().ToLower().Contains("serializer"))
-				Types.Add(node);
-		}
-
-		public override void VisitGenericName(GenericNameSyntax node)
-		{
-			if(node.ToFullString().ToLower().Contains("serializer"))
-				Types.Add(node);
-		}
-	}
-
 	internal abstract class BaseComplexArrayInvokationExpressionEmitter<TSerializerType>
 		: BaseArraySerializationInvokationExpressionEmitter<TSerializerType> 
 		where TSerializerType : BaseArraySerializerNonGenericMarker
