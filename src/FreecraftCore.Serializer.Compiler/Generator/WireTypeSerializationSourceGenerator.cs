095e40e3747176ca8ebb4214dd2746e4b64dc535
HelloKitty <andrew.blakely@ymail.com>
2020-12-16 02:48:37 -0600

    Added SerializerDebug folder feature

--- src/FreecraftCore.Serializer.Compiler/Generator/WireTypeSerializationSourceGenerator.cs
+++ src/FreecraftCore.Serializer.Compiler/Generator/WireTypeSerializationSourceGenerator.cs
@@ -36,8 +36,22 @@ namespace FreecraftCore.Serializer
 				SerializerSourceEmitter emitter = new SerializerSourceEmitter(symbols, outputStrategy, context.Compilation);
 				emitter.Generate();
 
-				foreach(var entry in outputStrategy.Content)
+				foreach (var entry in outputStrategy.Content)
 					context.AddSource(entry.Key, entry.Value);
+
+				//TODO: This is a hack but for some reason sometimes we end up writing serializers out to strange places.
+				if (Directory.GetCurrentDirectory().Contains(context.Compilation.AssemblyName))
+				{
+					//Write contents to Debug directory
+					string rootPath = Path.Combine("SerializerDebug");
+
+					if(!Directory.Exists(rootPath))
+						Directory.CreateDirectory(rootPath);
+
+					//Now we log out the serialization debug filers
+					foreach(var entry in outputStrategy.Content)
+						File.WriteAllText(Path.Combine(rootPath, $"{entry.Key}.cs"), entry.Value);
+				}
 			}
 			catch (System.Reflection.ReflectionTypeLoadException e)
 			{
