b8f3c28fd6d193ce6f6ed407a37aefcf5f45d5ed
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 10:11:43 -0600

    Renamed generator Type

--- src/FreecraftCore.Serializer.Compiler/Generator/WireGenerator.cs
+++ src/FreecraftCore.Serializer.Compiler/Generator/WireGenerator.cs
@@ -13,7 +13,7 @@ using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 namespace FreecraftCore.Serializer
 {
 	[Generator]
-	public sealed class WireTypeSerializationSourceGenerator : ISourceGenerator
+	public sealed class WireGenerator : ISourceGenerator
 	{
 		public void Initialize(GeneratorInitializationContext context)
 		{
@@ -38,34 +38,21 @@ namespace FreecraftCore.Serializer
 
 				foreach (var entry in outputStrategy.Content)
 					context.AddSource(entry.Key, entry.Value);
-
-				//TODO: This is a hack but for some reason sometimes we end up writing serializers out to strange places.
-				if (Directory.GetCurrentDirectory().Contains(context.Compilation.AssemblyName))
-				{
-					//Write contents to Debug directory
-					string rootPath = Path.Combine("SerializerDebug");
-
-					if(!Directory.Exists(rootPath))
-						Directory.CreateDirectory(rootPath);
-
-					//Now we log out the serialization debug filers
-					foreach(var entry in outputStrategy.Content)
-						File.WriteAllText(Path.Combine(rootPath, $"{entry.Key}.cs"), entry.Value);
-				}
 			}
 			catch (System.Reflection.ReflectionTypeLoadException e)
 			{
-				File.WriteAllText("Error.txt", $"{e}\n\nLoader: {e.LoaderExceptions.Select(ex => ex.ToString()).Aggregate((s1, s2) => $"{s1}\n{s2}")}");
+				context.AddSource("Error.txt", $"{e}\n\nLoader: {e.LoaderExceptions.Select(ex => ex.ToString()).Aggregate((s1, s2) => $"{s1}\n{s2}")}");
+				throw;
 			}
 			catch (Exception e)
 			{
-				File.WriteAllText("Error.txt", e.ToString());
+				context.AddSource("Error.txt", e.ToString());
 
 				try
 				{
 					context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("FCC001", "Compiler Failure", $"Error: {e.GetType().Name}. Failed: {e.Message} Stack: {{0}}", "Error", DiagnosticSeverity.Error, true), Location.None, BuildStackTrace(e)));
 				}
-				catch (Exception)
+				finally
 				{
 					throw e;
 				}
