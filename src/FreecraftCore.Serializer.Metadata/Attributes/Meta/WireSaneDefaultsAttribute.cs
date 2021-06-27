ef6cad8817e25e9ca171bd7c3e480c017f0b3335
HelloKitty <andrew.blakely@ymail.com>
2021-06-27 11:17:37 -0500

    Added WireSaneDefaultsAttribute

---
+++ src/FreecraftCore.Serializer.Metadata/Attributes/Meta/WireSaneDefaultsAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// Meta attribute that indicates to the serializer it should use SANE default serialization
+	/// behavior rather than the default World of Warcraft serialization/original FreecraftCore serialization.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public sealed class WireSaneDefaultsAttribute : Attribute
+	{
+
+	}
+}
