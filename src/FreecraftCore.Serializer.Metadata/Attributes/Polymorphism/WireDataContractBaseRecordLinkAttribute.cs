670be88a1f7e5aba78aaa39a7491c7fca0be7219
HelloKitty <andrew.blakely@ymail.com>
2021-06-27 05:41:48 -0500

    Added WireDataContractBaseRecordLinkAttribute

---
+++ src/FreecraftCore.Serializer.Metadata/Attributes/Polymorphism/WireDataContractBaseRecordLinkAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// Similar to <see cref="WireDataContractBaseLinkAttribute"/> but will use semantic analysis to determine
+	/// the key value. The default is to use the first constructor argument to the base record type
+	/// as the key value.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class WireDataContractBaseRecordLinkAttribute : Attribute
+	{
+
+	}
+}
