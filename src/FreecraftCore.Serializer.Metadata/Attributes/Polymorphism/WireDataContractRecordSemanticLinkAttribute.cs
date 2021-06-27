cc5316371c7e7ceee8dac8d099e8e4ec847cc738
HelloKitty <andrew.blakely@ymail.com>
2021-06-27 05:45:39 -0500

    Renamed WireDataContractRecordSemanticLinkAttribute

--- src/FreecraftCore.Serializer.Metadata/Attributes/Polymorphism/WireDataContractRecordSemanticLinkAttribute.cs
+++ src/FreecraftCore.Serializer.Metadata/Attributes/Polymorphism/WireDataContractRecordSemanticLinkAttribute.cs
@@ -5,12 +5,12 @@ using System.Text;
 namespace FreecraftCore.Serializer
 {
 	/// <summary>
-	/// Similar to <see cref="WireDataContractBaseLinkAttribute"/> but will use semantic analysis to determine
+	/// Similar to <see cref="WireDataContractBaseTypeAttribute"/> but will use semantic analysis to determine
 	/// the key value. The default is to use the first constructor argument to the base record type
 	/// as the key value.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class)]
-	public sealed class WireDataContractBaseRecordLinkAttribute : Attribute
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public sealed class WireDataContractRecordSemanticLinkAttribute : Attribute
 	{
 
 	}
