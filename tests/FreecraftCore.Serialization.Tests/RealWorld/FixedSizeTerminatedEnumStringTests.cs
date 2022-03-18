a2efc93de33845aaab3c88b00b0685cf4f1f9d0d
HelloKitty <andrew.blakely@ymail.com>
2022-03-18 01:17:03 -0500

    Create fixed-sized terminated enum string failure test cast

---
+++ tests/FreecraftCore.Serialization.Tests/RealWorld/FixedSizeTerminatedEnumStringTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using NUnit.Framework;
+
+namespace FreecraftCore.Serialization.Tests.RealWorld
+{
+	[TestFixture]
+	public class FixedSizeTerminatedEnumStringTests
+	{
+		[Test]
+		public void Test_Expected_Size_Is_Correct()
+		{
+			SerializerService service = new SerializerService();
+			Span<byte> span = new Span<byte>(new byte[100]);
+			int offset = 0;
+			service.Write(new TestModel(TestEnum.WoW), span, ref offset);
+
+			var array = span.Slice(0, offset).ToArray();
+			Assert.AreEqual(4, array.Length);
+		}
+	}
+
+	public enum TestEnum
+	{
+		WoW,
+		WCA
+	}
+
+	[WireMessageType]
+	[WireDataContract]
+	public sealed partial class TestModel
+	{
+		[Encoding(EncodingType.ASCII)]
+		[EnumString]
+		[KnownSize(4)]
+		[WireMember(1)]
+		public TestEnum TestEnumField;
+
+		public TestModel(TestEnum testEnumField)
+		{
+			TestEnumField = testEnumField;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public TestModel()
+		{
+			
+		}
+	}
+}
