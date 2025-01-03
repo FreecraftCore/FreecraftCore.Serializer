b481cd623f5029d1e8e60836539aca79f0615ab2
HelloKitty <andrew.blakely@ymail.com>
2025-01-03 00:40:38 -0600

    Create CodePage437StringTerminatorTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Terminator/CodePage437StringTerminatorTypeSerializerStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// CodePage437 null terminator encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class CodePage437StringTerminatorTypeSerializerStrategy : BaseStringTerminatorSerializerStrategy<CodePage437StringTerminatorTypeSerializerStrategy>
+	{
+		public CodePage437StringTerminatorTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.CodePage437)
+		{
+
+		}
+	}
+}
