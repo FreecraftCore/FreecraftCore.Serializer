750cdf9e9b6e6b6a947afeb2d675e88a6ad23edc
HelloKitty <andrew.blakely@ymail.com>
2025-01-03 00:44:07 -0600

    Create CodePage437StringTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Content/CodePage437StringTypeSerializerStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// CodePage437 encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class CodePage437StringTypeSerializerStrategy 
+		: BaseStringTypeSerializerStrategy<CodePage437StringTypeSerializerStrategy>, IFixedLengthCharacterSerializerStrategy
+	{
+		public CodePage437StringTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.CodePage437)
+		{
+			// This is useful for when you want to support the extended character set to include characters with accents.
+			// Ex. is chat messages which should be able to contain those.
+		}
+	}
+}
