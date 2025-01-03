f95dc74247a30e3e2d96535b24f8069df6b46c62
HelloKitty <andrew.blakely@ymail.com>
2025-01-03 02:27:18 -0600

    Create ISO8859StringTerminatorTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Terminator/ISO8859StringTerminatorTypeSerializerStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// ISO8859 null terminator encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class ISO8859StringTerminatorTypeSerializerStrategy 
+		: BaseStringTerminatorSerializerStrategy<ISO8859StringTerminatorTypeSerializerStrategy>
+	{
+		public ISO8859StringTerminatorTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.ISO8859)
+		{
+
+		}
+	}
+}
