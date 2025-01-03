bb156bf46e54e4e2d06f4c24767b0673e11b7630
HelloKitty <andrew.blakely@ymail.com>
2025-01-03 00:35:48 -0600

    Added CodePage437 encoding prop to CustomCharacterEncodingHelpers

---
+++ src/FreecraftCore.Serializer/Encoding/CustomCharacterEncodingHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public static class CustomCharacterEncodingHelpers
+	{
+		/// <summary>
+		/// See: https://en.wikipedia.org/wiki/Code_page_437
+		/// This is essentially the extended ASCII encoding. Meaning that you can encode characters like ñ.
+		/// </summary>
+		public static Encoding CodePage437 { get; } = Encoding.GetEncoding(437);
+	}
+}
