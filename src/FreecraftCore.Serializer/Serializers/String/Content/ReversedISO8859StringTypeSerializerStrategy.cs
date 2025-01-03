2889dce58791ab53070bdb3f15c1ed8bcf64b23a
HelloKitty <andrew.blakely@ymail.com>
2025-01-03 02:34:04 -0600

    Create ReversedISO8859StringTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Content/ReversedISO8859StringTypeSerializerStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// ISO8859 encoding implementation of string serialization using data reversal.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class ReversedISO8859StringTypeSerializerStrategy
+		: BaseReversedStringTypeSerializerStrategy<ReversedISO8859StringTypeSerializerStrategy>, IFixedLengthCharacterSerializerStrategy
+	{
+		public ReversedISO8859StringTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.ISO8859)
+		{
+			// If you REALLY need old CodePage437
+		}
+	}
+}
