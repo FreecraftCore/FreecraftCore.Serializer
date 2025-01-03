66dd921f225716e0fdb744b17d65d4dd902b5c2b
HelloKitty <andrew.blakely@ymail.com>
2025-01-03 00:44:58 -0600

    Create ReversedCodePage437StringTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Content/ReversedCodePage437StringTypeSerializerStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// CodePage437 encoding implementation of string serialization using data reversal.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class ReversedCodePage437StringTypeSerializerStrategy 
+		: BaseReversedStringTypeSerializerStrategy<ReversedCodePage437StringTypeSerializerStrategy>, IFixedLengthCharacterSerializerStrategy
+	{
+		public ReversedCodePage437StringTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.CodePage437)
+		{
+			//This is the default that WoW uses
+			//however now that the serializer is being used for other projects
+			//we need to have the option to use different ones.
+		}
+	}
+}
