b206eeba86740ccaa68512489649b6e3ace92b33
HelloKitty <andrew.blakely@ymail.com>
2025-03-13 15:22:53 -0500

    Create UCS2Encoding.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Encoding/UCS2Encoding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// A UCS-2 encoding class that extends UnicodeEncoding.
+	/// Effectively behaves like UTF-16 but assumes all characters are exactly 2 bytes (no surrogate pairs).
+	/// </summary>
+	public sealed class UCS2Encoding : UnicodeEncoding
+	{
+
+	}
+}
