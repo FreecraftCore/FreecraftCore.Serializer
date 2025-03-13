9f7eb050a938d162dfdee8bb44727f14dfa2d084
HelloKitty <andrew.blakely@ymail.com>
2025-03-13 15:37:34 -0500

    Create UCS2StringTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Content/UCS2StringTypeSerializerStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// UCS2 encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class UCS2StringTypeSerializerStrategy : BaseStringTypeSerializerStrategy<UCS2StringTypeSerializerStrategy>
+	{
+		public UCS2StringTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.UCS2)
+		{
+
+		}
+	}
+}
