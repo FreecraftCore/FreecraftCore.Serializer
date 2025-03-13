483e243f656594cb29f6fc0badd5d65e209e6b61
HelloKitty <andrew.blakely@ymail.com>
2025-03-13 15:38:15 -0500

    Create UCS2StringTerminatorTypeSerializerStrategy.cs

---
+++ src/FreecraftCore.Serializer/Serializers/String/Terminator/UCS2StringTerminatorTypeSerializerStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Serializer
+{
+	/// <summary>
+	/// UCS-2 null terminator encoding implementation of string serialization.
+	/// </summary>
+	[KnownTypeSerializer]
+	public sealed class UCS2StringTerminatorTypeSerializerStrategy : BaseStringTerminatorSerializerStrategy<UCS2StringTerminatorTypeSerializerStrategy>
+	{
+		public UCS2StringTerminatorTypeSerializerStrategy()
+			: base(CustomCharacterEncodingHelpers.UCS2)
+		{
+
+		}
+	}
+}
